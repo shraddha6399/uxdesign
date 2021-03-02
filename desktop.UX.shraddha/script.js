543ed20fa02b4ecc6acaa2e4356d8e36e57074c0
shraddha6399 <78547247+shraddha6399@users.noreply.github.com>
2021-03-02 16:56:32 +0530

    Add files via upload

---
+++ desktop.UX.shraddha/script.js
@@ -0,0 +1,15 @@
+// mobile top-menu
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+
+
+
+
